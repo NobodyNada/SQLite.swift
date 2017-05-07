6471f7024d57b143bf439c555466f659582ab77e
NobodyNada <jkeller234@icloud.com>
2017-05-06 21:10:23 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,16 +11,10 @@ let package = Package(
     exclude: ["Tests/CocoaPods", "Tests/Carthage"]
 )
 
-#if os(Linux)
-    package.exclude.append(contentsOf: [
-        "Sources/SQLiteObjc",
-        "Sources/SQLite/Extensions/FTS4.swift",
-        "Sources/SQLite/Extensions/FTS5.swift",
-        "Tests/SQLiteTests/FTS4Tests.swift",
-        "Tests/SQLiteTests/FTS5Tests.swift"
-    ])
-#else
-    let objcTarget = Target(name: "SQLiteObjc")
-    package.targets.first?.dependencies.append(.Target(name: objcTarget.name))
-    package.targets.append(objcTarget)
-#endif
+package.exclude.append(contentsOf: [
+    "Sources/SQLiteObjc",
+    "Sources/SQLite/Extensions/FTS4.swift",
+    "Sources/SQLite/Extensions/FTS5.swift",
+    "Tests/SQLiteTests/FTS4Tests.swift",
+    "Tests/SQLiteTests/FTS5Tests.swift"
+])
